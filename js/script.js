43c0d0e08ecbe8d9bf2f226b29e4d85fd1c3e583
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2024-07-13 17:41:16 -0300

    subindo botão de subir até o topo

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ function hideOverlay(card) {
 function keepOverlayVisible(overlay) {
     overlay.classList.add('show');
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
